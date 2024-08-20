8d10cdaf2b308d4648889e298797f439e6d8d83c
Sherry <you@example.com>
2024-08-19 23:17:24 -0500

    Game created

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,135 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/wordle.module.css';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
+const urlTwo = 'https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt'
+const WORD_LENGTH = 5; 
+
+export default function App() {
+  const [solution, setSolution] = useState('');
+  const [guesses, setGuesses] = useState(Array(6).fill(null))
+  const [currentGuess, setCurrentGuess] = useState(''); 
+  const [isGameOver, setIsGameOver] = useState(false)
+  const [wordList, setWordList] = useState([]); 
+  const [showSolution, setShowSolution] = useState(false)
+
+  useEffect(() => { 
+    const handleType = (event ) => { 
+
+      if (isGameOver) { 
+        alert('You got it!');
+        return; 
+      } 
+      if (event.key === 'Enter') { 
+        if (currentGuess.length !== 5) {  
+          return; 
+        }
+
+        if (!wordList.includes(currentGuess)) {
+          alert('Invalid Word!');
+          return; 
+        }
+
+        const newGuesses = [...guesses]; 
+        newGuesses[guesses.findIndex(val => val == null)] = currentGuess; 
+        setGuesses(newGuesses); 
+        setCurrentGuess('');
+
+        const isCorrect = solution === currentGuess; 
+        if (isCorrect) {
+          setIsGameOver(true); 
+
+        } 
+      }
+
+      if (event.key === 'Backspace') { 
+        setCurrentGuess(currentGuess.slice(0, -1));
+        return; 
+      }
+
+      if (currentGuess.length >=5) { 
+        return; 
+      }
+
+      const isLetter = event.key.match(/^[a-z]{1}$/) !=null; 
+      if (isLetter) {
+        setCurrentGuess(currentGuess + event.key);
+
+      }
+
+    };
+
+    window.addEventListener('keydown', handleType); 
+
+    return () => window.removeEventListener('keydown', handleType); 
+
+  }, [currentGuess, isGameOver, solution, guesses])
+
+
+  useEffect(() => {
+    const fetchWord = async () => { 
+      const response = await fetch(API_URL);
+      const words = await response.json(); 
+      const randomWord = words[Math.floor(Math.random() * words.length)];
+
+    };
+
+    const fetchWordTwo = async () => { 
+      const responseTwo = await fetch(urlTwo);
+      const wordsTwo = await responseTwo.text(); 
+      const array = wordsTwo.toString().split("\n")
+      const randomWordTwo = array[Math.floor(Math.random() * array.length)];
+      setSolution(randomWordTwo.toLowerCase());
+      setWordList(array.map(word => word.toLowerCase()));
+    }   
+
+
+    fetchWordTwo();
+
+  }, []); // use Effect dependecy so the array only runs once
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <>
+      <h1 className={styles.header}> WORDLE CLONE </h1>
+        <div className={styles.Board}>
+          {
+            guesses.map((guess, i )  => { 
+              const isCurrentGuess = i === guesses.findIndex(val => val == null); 
+              return (
+                <Line guess = {isCurrentGuess ? currentGuess : guess ?? ""} 
+                isFinal = {!isCurrentGuess && guess !=null} 
+                solution = {solution} 
+                key = {i}/> 
+              );
+            })
+          }
+          <button className={styles.button} onClick={() => setShowSolution(true)}> See Solution </button>
+          {showSolution && <div> {solution} </div> } 
         </div>
-      </main>
+    </>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+  ) 
+} 
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
+function Line({guess, isFinal, solution}) { 
+  const tiles = []; 
+  for (let i = 0; i < WORD_LENGTH; i++) { 
+    const char = guess[i]; 
+    let className = styles.Tile;
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+    if (isFinal) { 
+      if (char === solution[i]) { 
+        className += ` ${styles.correct}` ;
+      } else if (solution.includes(char)) {
+        className += ` ${styles.close}`;
+      } else { 
+        className += ` ${styles.incorrect}`; 
+      }
+
+    }
+
+    tiles.push(<div key={i} className={className}> {char} </div>)
+  }
+  return <div className={styles.Line}>{tiles}</div> 
 }
+
